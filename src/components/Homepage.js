e166d3a4d8ab9c6d89037ef8e6bb607acb234007
BintReina <omobowalerohma1998@gmail.com>
2022-11-16 07:10:03 +0100

    built the nft section

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "../styles/homepage.css";
 import "../styles/styles.css";
-import  Header from "../components/Header"
+import Header from "../components/Header";
 import Wallets from "../components/Wallets";
 import Houses from "../components/Houses";
-
+import NFTs from "../components/NFTs";
 
 function Homepage() {
   return (
     <div className="homepage">
       <Header />
       <Wallets />
-      <Houses/>
+      <Houses />
+      <NFTs />
     </div>
   );
 }
---
+++ src/components/NFTs.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../styles/nfts.css";
+import "../styles/styles.css";
+import Button from "./Button";
+import Nftimage from "../images/nft-image.svg";
+
+
+
+
+function NFTs() {
+  return (
+    <section className="nfts grid">
+      <div className="nfts-container">
+        <div className="nfts-text">
+          <h3>Metabnb NFTs</h3>
+          <p>
+            Discover our NFT gift cards collection. Loyal customers gets amazing
+            gift cards which are traded as NFTs. These NFTs gives our cutomer
+            access to loads of our exclusive services.
+          </p>
+          <Button name="Learn more" styleclass="btn-nft" />
+        </div>
+        <div className="nfts-images">
+          <img src={Nftimage} className="nft-image" alt="beautiful nft houses"/>
+           </div>
+      </div>
+    </section>
+  );
+}
+
+export default NFTs;
