42d3c4f9e209142b14fde3c174f8f453fcdf0c4e
BintReina <omobowalerohma1998@gmail.com>
2022-12-23 22:44:39 +0100

    refactored render props to hooks

--- src/Nav.js
+++ src/Nav.js
@@ -8,61 +8,62 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 // import withToggler from "./components/HOCS/withToggler";
-import Toggler from "./components/Toggler";
-function Nav() {
-  return (
-    <Toggler
-      renderprops={function (show, toggle) {
-        return (
-          <section className="nav--container grid">
-            <div className="nav--container_2">
-              <div className="nav-section_content">
-                <div className="logo--container">
-                  <img src={home} alt="a home icon" className="home--img" />
-                  <img src={logo} className="meta--logo" alt="metabnb logo" />
-                </div>
-                <nav>
-                  <ul className="nav-links">
-                    <Link to="/">
-                      {" "}
-                      <li className="nav-link">Home</li>
-                    </Link>
-                    <Link to="/place">
-                      <li className="nav-link">Place to stay</li>
-                    </Link>
-                    <li className="nav-link">NFTs</li>
-                    <li className="nav-link">Community</li>
-                  </ul>
-                </nav>
-                <div>
-                  {" "}
-                  <Button name="Connect wallet" styleclass="nav-btn" />
-                </div>
+// import Toggler from "./components/Toggler";
 
-                <div className="hamburger-menu" onClick={toggle}>
-                  <FontAwesomeIcon icon={faBars} className="hamburger-menu" />
-                </div>
-              </div>
-              {/* TOGGLE MENU LIST */}
-              {show ? (
-                <div className="mobile-menu_list">
-                  <Link to="/">
-                    <p onClick={toggle}>Home</p>
-                  </Link>
-                  <Link to="/place">
-                    <p onClick={toggle}>Place to stay</p>
-                  </Link>
-                  <p>NFTs</p>
-                  <p>Community</p>
-                </div>
-              ) : (
-                ""
-              )}
-            </div>
-          </section>
-        );
-      }}
-    />
+import { useState } from "react";
+function Nav() {
+  const [show, setShow] = useState(false);
+
+  function toggle() {
+    setShow(!show);
+  }
+  return (
+    <section className="nav--container grid">
+      <div className="nav--container_2">
+        <div className="nav-section_content">
+          <div className="logo--container">
+            <img src={home} alt="a home icon" className="home--img" />
+            <img src={logo} className="meta--logo" alt="metabnb logo" />
+          </div>
+          <nav>
+            <ul className="nav-links">
+              <Link to="/">
+                {" "}
+                <li className="nav-link">Home</li>
+              </Link>
+              <Link to="/place">
+                <li className="nav-link">Place to stay</li>
+              </Link>
+              <li className="nav-link">NFTs</li>
+              <li className="nav-link">Community</li>
+            </ul>
+          </nav>
+          <div>
+            {" "}
+            <Button name="Connect wallet" styleclass="nav-btn" />
+          </div>
+
+          <div className="hamburger-menu" onClick={toggle}>
+            <FontAwesomeIcon icon={faBars} className="hamburger-menu" />
+          </div>
+        </div>
+        {/* TOGGLE MENU LIST */}
+        {show ? (
+          <div className="mobile-menu_list">
+            <Link to="/">
+              <p onClick={toggle}>Home</p>
+            </Link>
+            <Link to="/place">
+              <p onClick={toggle}>Place to stay</p>
+            </Link>
+            <p>NFTs</p>
+            <p>Community</p>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </section>
   );
 }
 
